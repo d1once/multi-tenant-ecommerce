79b01e44d5bfdb4abc02e1a409fee0d30364f81c
d1once <dionisenja@gmail.com>
2025-06-09 13:57:34 +0200

    feat: customization

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,27 @@
 import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Input } from "@/components/ui/input";
+import { Progress } from "@/components/ui/progress";
+import { Textarea } from "@/components/ui/textarea";
 
 export default function Home() {
   return (
-    <>
-    <Button>Hello World</Button>
-    </>
+    <div className="flex flex-col gap-y-4">
+      <div>
+        <Button variant={"elevated"}>Hello World</Button>
+      </div>
+      <div>
+        <Input placeholder="Hello Bitch" />
+      </div>
+      <div>
+        <Progress value={50} />
+      </div>
+      <div>
+        <Textarea />
+      </div>
+      <div>
+        <Checkbox />
+      </div>
+    </div>
   );
 }
