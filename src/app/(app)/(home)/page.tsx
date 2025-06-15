54ba18bd703719346bbae59af05ed20fe01796d8
d1once <dionisenja@gmail.com>
2025-06-15 10:09:43 +0200

    feat: authentication

--- src/app/(app)/(home)/page.tsx
+++ src/app/(app)/(home)/page.tsx
@@ -1,3 +1,13 @@
+"use client";
+
+import { useQuery } from "@tanstack/react-query";
+
+import { useTRPC } from "@/trpc/client";
+
 export default function Home() {
-  return <div className="flex flex-col gap-y-4"></div>;
+  const trpc = useTRPC();
+  const { data } = useQuery(trpc.auth.session.queryOptions());
+  return (
+    <div className="flex flex-col gap-y-4">{JSON.stringify(data, null, 2)}</div>
+  );
 }
