b0ec496cd0b26b86256d3e116e2da58b5d8c73dc
d1once <dionisenja@gmail.com>
2025-07-01 16:28:52 +0200

    feat: Access Control

--- src/collections/Tenants.ts
+++ src/collections/Tenants.ts
@@ -1,7 +1,12 @@
+import { isSuperAdmin } from "@/lib/access";
 import type { CollectionConfig } from "payload";
 
 export const Tenants: CollectionConfig = {
   slug: "tenants",
+  access: {
+    create: ({ req }) => isSuperAdmin(req.user),
+    delete: ({ req }) => isSuperAdmin(req.user),
+  },
   admin: {
     useAsTitle: "slug",
   },
@@ -21,6 +26,9 @@ export const Tenants: CollectionConfig = {
       index: true,
       required: true,
       unique: true,
+      access: {
+        update: ({ req }) => isSuperAdmin(req.user),
+      },
       admin: {
         description:
           "This is the subdomain for th store (e.g [slug].funroad.com",
@@ -36,16 +44,21 @@ export const Tenants: CollectionConfig = {
       type: "text",
       required: true,
       admin: {
-        readOnly: true,
+        description: "Stripe Account ID assoicated with your shop",
+      },
+      access: {
+        update: ({ req }) => isSuperAdmin(req.user),
       },
     },
     {
       name: "stripeDetailsSubmitted",
       type: "checkbox",
       admin: {
-        readOnly: true,
         description:
           "You cannot create products until you submit your Stripe details",
+      },
+      access: {
+        update: ({ req }) => isSuperAdmin(req.user),
       },
     },
   ],
