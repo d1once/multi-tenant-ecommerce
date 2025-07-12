406549dc76a99d9f3da6533c600b26670b3f5cc1
d1once <dionisenja@gmail.com>
2025-07-12 12:53:16 +0200

    chore: updated payload version for stroage blob

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -68,7 +68,7 @@ export default buildConfig({
       collections: {
         media: true,
       },
-      token: process.env.BLOB_READ_WRITE_TOKEN!,
+      token: process.env.BLOB_READ_WRITE_TOKEN,
     }),
   ],
 });
