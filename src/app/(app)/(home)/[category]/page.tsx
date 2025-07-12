566ed28f076fadfa592d3613dbc2a8a04b4efb74
d1once <dionisenja@gmail.com>
2025-07-12 10:54:51 +0200

    chore: deployment

--- src/app/(app)/(home)/[category]/page.tsx
+++ src/app/(app)/(home)/[category]/page.tsx
@@ -11,6 +11,7 @@ interface Props {
   }>;
   searchParams: Promise<SearchParams>;
 }
+export const dynamic = "force-dynamic";
 
 const Page = async ({ params, searchParams }: Props) => {
   const { category } = await params;
